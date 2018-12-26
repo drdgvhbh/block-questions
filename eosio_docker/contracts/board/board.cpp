dc2b4f4e7811ff86f5524f5ff9c75688db5858a6
Ryan Lee <drdgvhbh@gmail.com>
2018-12-26 00:43:51 -0500

    Add title and content to board contract

--- eosio_docker/contracts/board/board.cpp
+++ eosio_docker/contracts/board/board.cpp
@@ -1,10 +1,16 @@
 #include "board.hpp"
 
-ACTION board::postquestion(name author) {
+board::board(name receiver, name code, datastream<const char *> ds)
+    : contract(receiver, code, ds), questions_(code, code.value){};
+
+ACTION board::postquestion(name author, const std::string &title,
+                           const std::string &content) {
     require_auth(author);
 
     questions_.emplace(author, [&](auto &question) {
+        auto hash_data = author.to_string() + title + content;
         question.primary_key_ = questions_.available_primary_key();
+        question.content_hash = sha256(hash_data.c_str(), hash_data.length());
         question.author = author;
     });
 }
--- eosio_docker/contracts/board/board.hpp
+++ eosio_docker/contracts/board/board.hpp
@@ -7,20 +7,27 @@ CONTRACT board : public contract {
    public:
     using contract::contract;
 
-    board(name receiver, name code, datastream<const char *> ds)
-        : contract(receiver, code, ds), questions_(code, code.value){};
+    board(name receiver, name code, datastream<const char *> ds);
 
-    ACTION postquestion(name author);
+    ACTION postquestion(name author, const std::string &title,
+                        const std::string &content);
 
    private:
     TABLE question {
         uint64_t primary_key_;
+        checksum256 content_hash;
         name author;
 
         uint64_t primary_key() const { return primary_key_; };
+
+        checksum256 get_content_hash() const { return content_hash; };
     };
 
-    typedef multi_index<name("question"), question> question_table;
+    typedef multi_index<name("question"), question,
+                        indexed_by<name("contenthash"),
+                                   const_mem_fun<question, checksum256,
+                                                 &question::get_content_hash>>>
+        question_table;
 
     question_table questions_;
 };
