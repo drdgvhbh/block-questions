91eb323826a1b5fb3062141166ae739540b04247
Ryan Lee <drdgvhbh@gmail.com>
2019-01-06 16:50:37 -0500

    Modify start script

--- eosio/scripts/start.sh
+++ eosio/scripts/start.sh
@@ -1,25 +1,45 @@
 #!/usr/bin/env bash
-WALLET_URL=http://keosd:8899
+
+WALLET_URL="http://keosd:8899"
 WAIT_HOSTS=keosd:8899 /wait
-cleos --wallet-url $WALLET_URL  wallet create --file wallet_password.txt
-cleos --wallet-url $WALLET_URL wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
-(nodeos -e -p eosio \
+
+DIR_TO_CHECK="data"
+WALLET_PASSWORD_FILE="data/wallet_password.txt"
+if [ -n "$(find "$DIR_TO_CHECK" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
+  cleos --wallet-url $WALLET_URL wallet create --file $WALLET_PASSWORD_FILE
+  cleos --wallet-url $WALLET_URL wallet import --private-key 5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3
+  (nodeos -e -p eosio \
   --plugin eosio::chain_api_plugin \
   --plugin eosio::net_api_plugin \
   --contracts-console \
-  --verbose-http-errors) &
+  --verbose-http-errors \
+  --config-dir data/eosio \
+  --data-dir data/eosio) &
+else
+  cleos --wallet-url $WALLET_URL wallet unlock --password $(cat $WALLET_PASSWORD_FILE)
+  (nodeos -e -p eosio \
+  --plugin eosio::chain_api_plugin \
+  --plugin eosio::net_api_plugin \
+  --hard-replay \
+  --contracts-console \
+  --verbose-http-errors \
+  --config-dir data/eosio \
+  --data-dir data/eosio) &
+fi
 until curl localhost:8888/v1/chain/get_info
 do
   echo "Running curl on localhost:8888/v1/chain/get_info"
   sleep 1s
 done
-cleos --wallet-url $WALLET_URL set contract eosio contracts/eosio.bios
-cleos create key --file wallet_key.txt
-PRIVATE_KEY=$(head wallet_key.txt -n 1 | grep -oE '[^ ]+$')
-PUBLIC_KEY=$(sed "2q;d" wallet_key.txt | grep -oE '[^ ]+$')
-PRODUCER_NAME="mongo"
-cleos --wallet-url $WALLET_URL wallet import --private-key $PRIVATE_KEY
-cleos --wallet-url $WALLET_URL create account eosio $PRODUCER_NAME $PUBLIC_KEY $PUBLIC_KEY
+PRODUCER_NAME="producer"
+if [ -n "$(find "$DIR_TO_CHECK/$PRODUCER_NAME" -maxdepth 0 -type d -empty 2>/dev/null)" ]; then
+  cleos --wallet-url $WALLET_URL set contract eosio contracts/eosio.bios
+  cleos create key --file wallet_key.txt
+  PRIVATE_KEY=$(head wallet_key.txt -n 1 | grep -oE '[^ ]+$')
+  PUBLIC_KEY=$(sed "2q;d" wallet_key.txt | grep -oE '[^ ]+$')
+  cleos --wallet-url $WALLET_URL wallet import --private-key $PRIVATE_KEY
+  cleos --wallet-url $WALLET_URL create account eosio $PRODUCER_NAME $PUBLIC_KEY $PUBLIC_KEY
+fi
 (nodeos --producer-name $PRODUCER_NAME \
   --plugin eosio::chain_api_plugin \
   --plugin eosio::net_api_plugin \
@@ -31,16 +51,25 @@ cleos --wallet-url $WALLET_URL create account eosio $PRODUCER_NAME $PUBLIC_KEY $
   --data-dir data/$PRODUCER_NAME) &
 cleos --wallet-url $WALLET_URL \
   push action eosio setprods "{ \"schedule\": [{\"producer_name\": \"$PRODUCER_NAME\",\"block_signing_key\": \"$PUBLIC_KEY\"}]}" -p eosio@active
-PRODUCER_2=derp
-cleos --wallet-url $WALLET_URL create account eosio $PRODUCER_2 $PUBLIC_KEY $PUBLIC_KEY
-(nodeos -e -p $PRODUCER_2 \
+MONGO_PRODUCER_NAME=mongo
+
+cleos --wallet-url $WALLET_URL create account eosio $MONGO_PRODUCER_NAME $PUBLIC_KEY $PUBLIC_KEY
+until curl mongo:27017
+do
+  sleep 1s
+done
+(nodeos \
   --plugin eosio::chain_api_plugin \
   --plugin eosio::net_api_plugin \
+  --plugin eosio::mongo_db_plugin \
+  --read-mode "read-only" \
   --http-server-address 127.0.0.1:8890 \
   --p2p-listen-endpoint localhost:9878 \
   --p2p-peer-address localhost:9877 \
-  --config-dir data/$PRODUCER_2 \
-  --data-dir data/$PRODUCER_2) &
+  --config-dir data/$MONGO_PRODUCER_NAME \
+  --data-dir data/$MONGO_PRODUCER_NAME \
+  --mongodb-uri mongodb://mongo:27017/EOS \
+  --mongodb-update-via-block-num true) &
 while :
   do
     sleep 1
